c20caf357fed451e37caf1fc446e5b6ef371614b
Joseph Nguyen <joseph.h.nguyen@sjsu.edu>
2025-06-04 14:08:35 -0700

    Fix eslint errors again

--- frontend/app/api/send/route.ts
+++ frontend/app/api/send/route.ts
@@ -1,22 +1,22 @@
-import { Resend } from 'resend';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'bossyglobalinc@gmail.com',
-      to: ['jthnguyen02@gmail.com'],
-      subject: 'Hello world',
-      html: '<p>Congrats on sending your first email!</p>'
-    });
+// export async function POST() {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: 'bossyglobalinc@gmail.com',
+//       to: ['jthnguyen02@gmail.com'],
+//       subject: 'Hello world',
+//       html: '<p>Congrats on sending your first email!</p>'
+//     });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+//     if (error) {
+//       return Response.json({ error }, { status: 500 });
+//     }
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
